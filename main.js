a132b61066477e020977092d90162cc54b9bb70d
Arrafa556 <arrafaaddress@gmail.com>
2026-05-01 14:27:01 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('selamat datang di belajar javascript');
+document.writeln('<h1>no Llevo mucho tiempo JavaScript</h1>');
+document.writeln('<p>javascript es un lenguaje de programación que hace que los sitios web</p>');
+document.writeln('<h2>Aprender js es fácil y también tiene su arte</h2>');
+document.writeln('<p>javascript es un lenguaje de programación que hace que los sitios web sean interactivos</p>');
